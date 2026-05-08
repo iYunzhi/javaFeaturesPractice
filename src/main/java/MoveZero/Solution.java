ef9bcc5f04c7677a25b5ce142b2b4f03a665ba29
welt <welty898@gmail.com>
2026-05-08 17:25:53 +0800

    移动零修正

--- src/main/java/MoveZero/Solution.java
+++ src/main/java/MoveZero/Solution.java
@@ -4,14 +4,13 @@ import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {0,1,0,3,12};
+        int[] nums = {1,0};
         moveZeroes(nums);
         System.out.println(Arrays.toString(nums));
     }
     public static void moveZeroes(int[] nums) {
-        if(nums.length == 1 || nums.length == 0){return;}
         int k = 0;
-        for(int i = 1; i<nums.length;i++ ){
+        for(int i = 0; i<nums.length;i++){
             if(nums[i] != 0){
                 nums[k++] = nums[i];
             }
