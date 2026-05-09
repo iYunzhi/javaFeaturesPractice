e726a9bd8f5684cfcff933b523c657db1a2a23e7
welt <welty898@gmail.com>
2026-05-09 14:36:01 +0800

    fix:路径标准化

--- src/main/java/SwapNodesInpairs/ListNode.java
+++
@@ -1,9 +0,0 @@
-package SwapNodesInpairs;
-
-public class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
--- src/main/java/quizForEnum/Super.java
+++
@@ -1,4 +0,0 @@
-package quizForEnum;
-
-public class Super {
-}
