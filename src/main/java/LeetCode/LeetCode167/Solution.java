97d4da9cc00787bbadfb791302b068efe2908cd6
welt <welty898@gmail.com>
2026-05-15 13:59:40 +0800

    两数之和 II

---
+++ src/main/java/LeetCode/LeetCode167/Solution.java
@@ -0,0 +1,25 @@
+package LeetCode.LeetCode167;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.twoSum(new int[]{2, 7, 11, 15}, 9)));
+    }
+
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left+1, right+1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
