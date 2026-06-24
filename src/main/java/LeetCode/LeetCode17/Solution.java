aa4d18f7350453ce6b214e430fbb6afc6d2d855a
iYunZhi <welty898@gmail.com>
2026-06-24 23:18:49 +0800

    电话号码的数字组合

---
+++ src/main/java/LeetCode/LeetCode17/Solution.java
@@ -0,0 +1,38 @@
+package LeetCode.LeetCode17;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    private static final String[] MAPPRING = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
+
+        int n = digits.length();
+        if (n == 0){
+            return ans;
+        }
+        char[] path = new char[n];
+
+        dfs(digits,0,ans,path);
+
+        return ans;
+    }
+
+    private void dfs(String digits, int i, List<String> ans, char[] path) {
+        if (i == digits.length()){
+            ans.add(new String(path));
+            return;
+        }
+
+        int index = digits.charAt(i) - '0';
+        String letter = MAPPRING[index];
+
+        for (char c : letter.toCharArray()) {
+            path[i] = c;
+            dfs(digits,i+1,ans,path);
+        }
+
+    }
+}
